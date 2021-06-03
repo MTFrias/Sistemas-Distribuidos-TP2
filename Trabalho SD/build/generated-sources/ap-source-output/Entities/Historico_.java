d7a6cbb411b764bede139974b21bb7e9383820d2
Frias <miguelfrias16@gmail.com>
2021-06-03 11:39:11 +0100

    Registo e Login
    powered by Vinicius

---
+++ Trabalho SD/build/generated-sources/ap-source-output/Entities/Historico_.java
@@ -0,0 +1,18 @@
+package Entities;
+
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-06-03T11:30:41")
+@StaticMetamodel(Historico.class)
+public class Historico_ { 
+
+    public static volatile SingularAttribute<Historico, Integer> idUtilizador;
+    public static volatile SingularAttribute<Historico, Double> total;
+    public static volatile SingularAttribute<Historico, Integer> idProduto;
+    public static volatile SingularAttribute<Historico, Date> data;
+    public static volatile SingularAttribute<Historico, Integer> idHistorico;
+
+}
---
+++ Trabalho SD/src/java/Beans/SessionBean.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Beans;
+
+import javax.ejb.Singleton;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author vinciusrodriguessilvacosta
+ */
+@Singleton
+public class SessionBean {
+
+    @PersistenceContext(unitName = "Trabalho_SDPU")
+    private EntityManager em;
+    
+    String email;
+    String password;
+    int id;
+
+    public void persist(Object object) {
+        em.persist(object);
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    
+}
